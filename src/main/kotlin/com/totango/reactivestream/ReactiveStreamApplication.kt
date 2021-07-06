096a6dc893f39210e88b3fe0b28ab0b0178726a1
Barak Bar Orion <barak.bar@gmail.com>
2021-07-06 19:49:16 +0300

    Initial commit

---
+++ src/main/kotlin/com/totango/reactivestream/ReactiveStreamApplication.kt
@@ -0,0 +1,11 @@
+package com.totango.reactivestream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveStreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveStreamApplication>(*args)
+}
---
+++ src/test/kotlin/com/totango/reactivestream/ReactiveStreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.totango.reactivestream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveStreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
